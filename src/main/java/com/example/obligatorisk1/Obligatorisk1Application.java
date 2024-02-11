3f0dfbd8af1e1d63cd8f1898c5b4c7f6ced96986
duaaadalh <142824887+duaaadalh@users.noreply.github.com>
2024-02-11 21:31:36 +0100

    initial commit

---
+++ src/main/java/com/example/obligatorisk1/Obligatorisk1Application.java
@@ -0,0 +1,13 @@
+package com.example.obligatorisk1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Obligatorisk1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Obligatorisk1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/obligatorisk1/Obligatorisk1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.obligatorisk1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Obligatorisk1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
